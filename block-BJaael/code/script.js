daa8a9882b778dc6a538a87ca9731f8e2f893bc2
rish0229 <sushantsareen456@gmail.com>
2020-09-25 14:37:16 +0530

    refactor

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,10 +1,21 @@
-let randomHsl = () =>
-  `rgb(${Math.random() * 360}, ${Math.random() * 360}, ${Math.random() * 360})`;
 let boxes = document.querySelector('.boxes');
+
+for (let i = 0; i < 500; i++) {
+  let newBox = document.createElement('div');
+  newBox.classList.add('box');
+  boxes.append(newBox);
+}
+
 let boxElm = document.querySelectorAll('.box');
+
+let randomHsl = () =>
+  `rgba(${Math.random() * 360}, ${Math.random() * 360}, ${
+    Math.random() * 360
+  }, 0.5)`;
+
 boxes.addEventListener('mouseover', () => {
   for (let box of boxElm) {
     box.style.background = randomHsl();
-    box.innerText = Math.floor(Math.random() * 500 + 1);
+    box.innerHTML = `<h3>${Math.floor(Math.random() * 501)}</h3>`;
   }
 });
