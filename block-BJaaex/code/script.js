b1fc219831938d3169a1839d2c7a48004652413f
rish0229 <sushantsareen456@gmail.com>
2020-09-30 18:02:31 +0530

    incomplete

--- block-BJaaex/code/script.js
+++ block-BJaaex/code/script.js
@@ -1,58 +1,19 @@
-// 1. Change the title of the page to `Hello AltCampus!`
+let parent = document.querySelector('.parent');
 
-// 2. Select the element using the children property:
+for (let i = 0; i < 16; i++) {
+  createUI();
+}
 
-//    - Select the `h1` element and change the value to `Learning DOM`
+function randomNumber(elm) {
+  elm.innerText = Math.floor(Math.random() * 10);
+  elm.style.color = 'white';
+}
 
-//    - Select the first `li` element inside the `ul` with class `topics` and change the innerText to `all about document`
-//    - Select the input element with name `email`
-
-// 3. Log the number (using console.log) of children of all the `li` element inside the ul with class `topics`
-
-// 4. Select the first input using the `type` selector and store them in variable named `emailInput`
-
-// 5. Select the ul element using class selector and store in `topics`
-
-// 6. Select the first label element and store in `label`
-
-// 7. Select the input of type `checkbox` with the `id` selector and store in `inputCheckbox`
-
-// 8. Select the input of type password using Attribute selectors. (eg: input[type="text"]) and store in `password`
-
-// 9. Select the input using the placeholder attribute selector with value `password` and store in `attrPassword`
-
-// 10. Select all the `li` element and store in `allTopics`
-
-// 11. Select all the input element of any type and store in `allInput`
-
-// 12. Use forEach to console the `innerText` property of all the li element in `allTopics` variable.
-
-// 13. Select all the elements with class `list` and store in variable `listOfSelectedTopics`
-
-// 14. Select the first li element inside the `ul` element using `>` (direct child) and store in `firstLi`
-
-// 15. Select all the img element and log the number of element saying `The total number of img element is ---`
-
-// 16. Select all the `p` element and store in `allPElement`
-
-// 17. Select all the buttons and log the count of buttons.
-
-// 18. Select all the `label` element and log the count.
-
-// 19. Select all the elements with `id` of `test`
-
-// 20. Select the first element with id `test` using `getElementById`
-
-// 21. Select the parent element of the element stored in `topics` variable (#5) and log the element.
-
-// 22. Select the next element sibling of the element stored in `topics` variable (#5) and log the element.
-
-// 23. Select the previous element sibling of the element stored in `topics` variable (#5) and change the `innerText` property to `Learning About Walking the DOM`.
-
-// 24. Select the first element child of the element stored in `topics` variable (#5) and change the `innerText` property of the element to `This is the first child element`.
-
-// 25. Select the last element child of the element stored in `topics` variable (#5) and log the `typeof` the element.
-
-// 26. Select the element with type `fieldset` and store in a variable named `fieldsetElm`.
-
-// 27. Select the parent element of the element stored in `fieldsetElm` variable (#5) and log the `typeof` the element.
+function createUI() {
+  let box = document.createElement('div');
+  box.classList.add('box', 'fa');
+  box.addEventListener('click', () => {
+    randomNumber(box);
+  });
+  parent.append(box);
+}
